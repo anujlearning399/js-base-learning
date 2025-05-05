0e72be6c77c20cbe80c8494eb7bbbabe380a1740
anujlearning399 <hoodaboy36@gmail.com>
2025-05-05 10:07:17 +0000

    variables types done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 636363
+let accountEmail = "hoodaboy36@gmail.com"
+var accountPassword = "6311111"
+accountCity = "Bangalore"
+
+//accountId = 2
+accountEmail = "anujhooda39"
+accountPassword = "630000"
+accountCity = "Delhi"
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword]);
